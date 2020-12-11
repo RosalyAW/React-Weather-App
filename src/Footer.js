893141468b9fb0fc32d42510e97248509f4f9bd1
RosalyAW <72017310+RosalyAW@users.noreply.github.com>
2020-12-11 17:29:14 +0100

    Added link to Footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function Footer() {
-  return <footer> Coded by Rosaly Wiegmans and open-sourced on GitHub</footer>;
+  return (
+    <div>
+      <footer> Coded by Rosaly Wiegmans and open-sourced on
+    <a href="https://github.com/RosalyAW/React-Weather-App" rel="noreferrer"
+          target="_blank"> Github </a>
+  </footer >
+  </div>
+  );
 }
